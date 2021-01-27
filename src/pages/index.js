7c6b71bf09bef116481984846c9fc387b5f2d316
solnishshka <testsolnishshka@yandex.ru>
2021-01-27 13:17:31 +0300

    some fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,38 @@
-import './index.css';
+import './index.css'
 
-const topMenu = document.querySelector('.menu_type_top');
-const openMenuSelector = 'menu_open';
-const menuButton = document.querySelector('.button_type_menu');
-const menuIcon = document.querySelector('.fa');
-const menuButtonClass = 'fa-bars';
-const menuCloseButtonClass = 'fa-times';
+const searchElement = document.querySelector('.header__search')
+const logo = document.querySelector('.header__logo');
+const headerTitle = document.querySelector('.header__title');
+const searchButton = document.querySelector('.button_type_search-icon')
+const topMenu = document.querySelector('.menu_type_top')
+const openMenuSelector = 'menu_open'
+const menuButton = document.querySelector('.button_type_menu')
+const menuIcon = document.querySelector('.fa')
+const menuButtonClass = 'fa-bars'
+const menuCloseButtonClass = 'fa-times'
 
-function handleOpenMenu(){
-    topMenu.classList.toggle(openMenuSelector);
-    if (menuIcon.classList.contains('fa-bars')){
-        menuIcon.classList.remove(menuButtonClass);
-    menuIcon.classList.add(menuCloseButtonClass);
-    }
-    else {
-        menuIcon.classList.add(menuButtonClass);
-        menuIcon.classList.remove(menuCloseButtonClass);
-    }
+function handleToggleMenu() {
+  topMenu.classList.toggle(openMenuSelector)
+  if (menuIcon.classList.contains('fa-bars')) {
+    menuIcon.classList.remove(menuButtonClass)
+    menuIcon.classList.add(menuCloseButtonClass)
+  } else {
+    menuIcon.classList.add(menuButtonClass)
+    menuIcon.classList.remove(menuCloseButtonClass)
+  }
+}
+
+function handleToggleSearch() {
+  searchElement.classList.toggle('header__search_visible')
+  logo.classList.toggle('header__logo_invisible');
+  menuButton.classList.toggle('button_invisible');
+  headerTitle.classList.toggle('header__title_invisible');
 }
 
 menuButton.addEventListener('click', () => {
-    handleOpenMenu();
+  handleToggleMenu()
 })
 
+searchButton.addEventListener('click', () => {
+  handleToggleSearch()
+})
